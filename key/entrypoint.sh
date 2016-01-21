3b15abb1e7da1d94e49ae7553777ed3a3a12b55e
EvgeneOskin <eoskin@crystalnix.com>
2016-01-22 01:40:23 +0600

    Complete auth with key.

--- key/entrypoint.sh
+++ key/entrypoint.sh
@@ -6,10 +6,12 @@ create_user() {
 }
 
 add_credential() {
+    passwd -d $1
     mkdir -p /home/$1/.ssh/
     cat /tmp/id_rsa.pub > /home/$1/.ssh/authorized_keys
     chmod 700 /home/$1/.ssh
     chmod 600 /home/$1/.ssh/authorized_keys
+    chown -R $1:remote /home/$1/.ssh/
 }
 
 mkdir /var/run/sshd
