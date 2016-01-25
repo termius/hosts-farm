5dd026d0542a6c05e80fe3e0ffec33d2a5e5ea25
EvgeneOskin <eoskin@crystalnix.com>
2016-01-25 13:46:06 +0600

    Fix typo in set password script.

--- authypass/set_password.sh
+++ authypass/set_password.sh
@@ -2,7 +2,7 @@
 
 for admin in $ADMIN_LIST
 do
-    ADMIN_ITEM=($(echo $i | tr ":" " "));
+    ADMIN_ITEM=($(echo $admin | tr ":" " "));
     echo "Set pass for ${ADMIN_ITEM[0]}"
     passwd ${ADMIN_ITEM[0]};
 done
