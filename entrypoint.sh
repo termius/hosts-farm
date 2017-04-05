2fcac203f51c1b2d07f7f2a874d09eb5a03d9553
Maxbey <maximbeiner@gmail.com>
2017-04-05 21:30:28 +0600

    Configs refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,13 @@ create_user() {
 add_credential() {
     passwd -d $1
     mkdir -p /home/$1/.ssh/
-    cat /tmp/id_ed25519.pub > /home/$1/.ssh/authorized_keys
+    cat /tmp/$PUB_KEY_NAME > /home/$1/.ssh/authorized_keys
     chmod 700 /home/$1/.ssh
     chmod 600 /home/$1/.ssh/authorized_keys
     chown -R $1:remote /home/$1/.ssh/
 }
+
+envsubst < /tmp/$CONFIG > "/etc/ssh/sshd_config"
 
 mkdir /var/run/sshd
 
